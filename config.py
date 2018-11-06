30638e97e27ca1853714229d821afaf151435b1b
prateekj117 <prateekj1171998@gmail.com>
2018-11-06 13:39:21 +0530

    Set up postgresql database named zhcet_coders with user prateek and password 12345 and made necessary changes in config.py

---
+++ config.py
@@ -0,0 +1,29 @@
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+
+class Config(object):
+    DEBUG = True
+    TESTING = False
+    CSRF_ENABLED = True
+    SECRET_KEY = 'this-really-needs-to-be-changed'
+    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
+
+
+class ProductionConfig(Config):
+    DEBUG = False
+
+
+class StagingConfig(Config):
+    DEVELOPMENT = True
+    DEBUG = True
+
+
+class DevelopmentConfig(Config):
+    DEVELOPMENT = True
+    DEBUG = True
+    SQLALCHEMY_DATABASE_URI = 'postgresql://localhost/zhcet_coders'
+
+
+class TestingConfig(Config):
+    TESTING = True
